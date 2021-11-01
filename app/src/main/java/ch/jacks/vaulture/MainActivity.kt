a1f9613761379c7c5db50ee6352b90da271e9975
Daniel Moreira <dani.moreira30@gmail.com>
2021-11-01 11:15:05 +0100

    Task 15 : Added screen protection

--- app/src/main/java/ch/jacks/vaulture/MainActivity.kt
+++ app/src/main/java/ch/jacks/vaulture/MainActivity.kt
@@ -1,12 +1,14 @@
 package ch.jacks.vaulture
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
     }
